8f9998bfaf1b316da3be4e93cf1f95abae89a082
Drew Thompson <drewallynthompson@gmail.com>
2021-01-16 09:52:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 function createEmployeeRecord(arr){
-  this.firstName = arr[0];
-  this.familyName = arr[1];
-  this.title = arr[2];
-  this.payPerHour = arr[3];
-  this.timeInEvents = [];
-  this.timeOutEvents = [];
+  let record = {
+    firstName: arr[0],
+    familyName: arr[1],
+    title: arr[2],
+    payPerHour: arr[3],
+    timeInEvents: [], 
+    timeOutEvents: []
+  }
+  return record;
 }
 
 function createEmployeeRecords(arr){
