1a21d61d09020693e7f388ef26b3714d4e2a01d0
Drew Thompson <drewallynthompson@gmail.com>
2021-01-16 08:43:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /* Your Code Here */
 class record{
-  constructor(firstName)
+  constructor(firstName, familyName,)
 }
 /*
  We're giving you this function. Take a look at it, you might see some usage
